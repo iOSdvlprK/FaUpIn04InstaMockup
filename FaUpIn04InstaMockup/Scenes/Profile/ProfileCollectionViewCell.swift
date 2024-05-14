8028533c37c456efe12d6dda7fa1b3b284f80390
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-05-15 00:21:35 +0900

    implement profile screen

---
+++ FaUpIn04InstaMockup/Scenes/Profile/ProfileCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileCollectionViewCell.swift
+//  FaUpIn04InstaMockup
+//
+//  Created by joe on 5/14/24.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileCollectionViewCell: UICollectionViewCell {
+    private let imageView = UIImageView()
+    
+    func setup(with image: UIImage) {
+        addSubview(imageView)
+        imageView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        
+        imageView.backgroundColor = .tertiaryLabel
+    }
+}
